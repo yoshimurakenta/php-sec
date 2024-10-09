89b5c2a1756a5c554868fccf1bb17715e22f0653
yoshimurakenta <y12kenta@gmail.com>
2024-10-09 16:39:15 +0900

    test_appの追加

---
+++ test_app/edit.php
@@ -0,0 +1,30 @@
+<?php
+require_once('functions.php');
+$todo = getSelectedTodo($_GET['id']);
+var_dump($_GET);
+
+setToken();
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>編集</title>
+</head>
+<body>
+  <?php if (!empty($_SESSION['err'])): ?>
+    <p><?= $_SESSION['err']; ?></p>
+  <?php endif; ?>
+  <form action="store.php" method="post">
+    <input type="hidden" name="token" value="<?= $_SESSION['token']; ?>">
+    <input type="hidden" name="id" value="<?= e($_GET['id']); ?>">
+    <input type="text" name="content" value="<?= e($todo); ?>">
+    <input type="submit" value="更新">
+  </form>
+  <div>
+    <a href="index.php">一覧へもどる</a>
+  </div>
+  <?php unsetError(); ?>
+</body>
+</html>
